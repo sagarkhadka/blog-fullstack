801f8218b97b77512212fe0360338ae6f81e3a83
Sagar Khadka <khadkasagar109@gmail.com>
2023-10-05 00:05:34 +0545

    timestamp

--- server/modules/blog/blog.model.js
+++ server/modules/blog/blog.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Please enter title']
-  },
+const blogSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please enter title']
+    },
 
-  description: {
-    type: String,
-    required: [true, 'Please enter description']
-  },
+    description: {
+      type: String,
+      required: [true, 'Please enter description']
+    },
 
-  body: {
-    type: String,
-    required: [true, 'Please enter blog body']
-  }
-})
+    body: {
+      type: String,
+      required: [true, 'Please enter blog body']
+    }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('blog', blogSchema)
